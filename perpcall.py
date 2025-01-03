66ce3294035a10927bd4acf8ce27be13cc275893
rohin20 <rohinphukan@gmail.com>
2025-01-03 12:36:45 -0800

    api call

--- perpcall.py
+++ perpcall.py
@@ -1,3 +1,5 @@
+from openai import OpenAI
+
 apifile = 'apikey'
 
 def get_file_contents(filename):
@@ -10,5 +12,33 @@ def get_file_contents(filename):
     except FileNotFoundError:
         print("'%s' file not found" % filename)
 
-api_key = get_file_contents(apifile)
-print(f"api_key = {api_key}")
+API_KEY = get_file_contents(apifile)
+
+
+messages = [
+    {
+        "role": "system",
+        "content": (
+            "You are an artificial intelligence assistant and you need to "
+            "provide concise answers to the user"
+        ),
+    },
+    {   
+        "role": "user",
+        "content": (
+            "does perplexity api have access to the internet?"
+        ),
+    },
+]
+
+client = OpenAI(api_key=API_KEY, base_url="https://api.perplexity.ai")
+
+# chat completion without streaming
+response = client.chat.completions.create(
+    model="llama-3.1-sonar-small-128k-online",
+    messages=messages,
+)
+print(response.choices[0].message.content)
+
+
+
