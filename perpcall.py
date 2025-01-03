37071287fb7e2774fd23c680bd27cc4eb4e1a931
rohin20 <rohinphukan@gmail.com>
2025-01-03 11:59:58 -0800

    apikey

--- perpcall.py
+++ perpcall.py
@@ -1,1 +1,14 @@
-print("hello world")
+apifile = 'apikey'
+
+def get_file_contents(filename):
+    """ Given a filename,
+        return the contents of that file
+    """
+    try:
+        with open(filename, 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        print("'%s' file not found" % filename)
+
+api_key = get_file_contents(apifile)
+print(f"api_key = {api_key}")
